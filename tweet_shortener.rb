8220a2c65f3980d1cbd26e47920860b29a45b220
david landau <thelandaus@gmail.com>
2018-05-11 17:29:08 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,34 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+
+  {
+    'hello' => 'hi',
+    'to' => '2', 
+    'two' => '2', 
+    'too' => '2',
+    'for' => '4', 
+    'four' => '4',
+    'be' => 'b',
+    'you' => 'u',
+    'at' => "@",
+    'and' => "&"
+  }
+
+end
+
+def word_substituter(tweet)
+  tweet_array = tweet.split
+  #binding.pry
+  tweet_array.collect do |word|
+    if dictionary[word.downcase]
+      word = dictionary[word.downcase]
+    else
+      word 
+    end
+  end.join(' ')
+end
+
+def bulk_tweet_shortener(tweet)
+  puts word_substituter(tweet)
+end
